59afd060022c13097860385f1057b60cadfd5e75
Abhinay Kumar <abhi01.amu@gmail.com>
2021-05-08 21:01:47 +0530

    api integration test successfull

--- server/app.js
+++ server/app.js
@@ -2,16 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { MD5 } = require('crypto-js');
+const axios = require('axios');
 
 const public_key = process.env.Public_Key;
 const private_key = process.env.Private_Key;
 
-app.get('/', (req, res) => {
+const getData = async (req, res, next) => {
   const timestamp = new Date().getTime().toString();
-  console.log(MD5(timestamp, private_key, public_key).toString());
-  return res.status(200).json({
-    status: 'success',
-  });
+  const str = timestamp + private_key + public_key;
+  const hash = MD5(str).toString();
+  let response = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=75737056736c29d701a9ea7d7edeb778&hash=${hash}&name=iron%20man`
+  );
+  //console.log(response.data.data.results[0].thumbnail.path);
+  res.send(`<img src="${response.data.data.results[0].thumbnail.path}.jpg" />`);
+  next();
+};
+
+app.get('/', getData, (req, res) => {
+  //   return res.status(200).json({
+  //     status: 'success',
+  //   });
 });
 
 app.listen(5000, () => console.log('server is running'));
