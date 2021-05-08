17721c1c9c82f0a5066547761f0b49486c5dcbc1
Abhinay Kumar <abhi01.amu@gmail.com>
2021-05-08 19:47:28 +0530

    client added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const { MD5 } = require('crypto-js');
+
+const public_key = process.env.Public_Key;
+const private_key = process.env.Private_Key;
 
 app.get('/', (req, res) => {
+  const timestamp = new Date().getTime().toString();
+  console.log(MD5(timestamp, private_key, public_key).toString());
   return res.status(200).json({
     status: 'success',
   });
