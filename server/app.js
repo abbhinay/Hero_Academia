c67f210b512fba2eb80b4fcd1f5b21799523d104
Abhinay Kumar <abhi01.amu@gmail.com>
2021-05-10 22:11:36 +0530

    added extra routes for server

--- server/app.js
+++ server/app.js
@@ -1,28 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const { MD5 } = require('crypto-js');
-const axios = require('axios');
+const searchComics = require('./routes/searchComics');
+const autocomplete = require('./routes/autocomplete');
+const searchCharacter = require('./routes/searchCharacter');
 
-const public_key = process.env.Public_Key;
-const private_key = process.env.Private_Key;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api/search', searchComics);
+app.use('/api/autocomplete', autocomplete);
+app.use('/api/character', searchCharacter);
 
-const getData = async (req, res, next) => {
-  const timestamp = new Date().getTime().toString();
-  const str = timestamp + private_key + public_key;
-  const hash = MD5(str).toString();
-  let response = await axios.get(
-    `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=75737056736c29d701a9ea7d7edeb778&hash=${hash}&name=iron%20man`
-  );
-  //console.log(response.data.data.results[0].thumbnail.path);
-  res.send(`<img src="${response.data.data.results[0].thumbnail.path}.jpg" />`);
-  next();
-};
-
-app.get('/', getData, (req, res) => {
-  //   return res.status(200).json({
-  //     status: 'success',
-  //   });
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    status: 'success',
+  });
 });
 
 app.listen(5000, () => console.log('server is running'));
