2037d026622d9bcc90f692202321d97d8e2ed9b1
Abhinay Kumar <abhi01.amu@gmail.com>
2021-05-16 16:56:33 +0530

    autocomplete functionality almost over

--- client/src/components/AutoComplete.js
+++ client/src/components/AutoComplete.js
@@ -1,32 +1,65 @@
+import { useState, useEffect } from 'react';
 import { Autocomplete, TextInput } from 'evergreen-ui';
 import axios from 'axios';
+import './AutoComplete.css';
 
 const AutoComplete = (props) => {
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState([]);
+  const [hero, setHero] = useState({});
+
+  useEffect(() => {
+    console.log(hero);
+  }, [hero]);
+
+  const getItem = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:5000/api/autocomplete/${value}`
+      );
+      setItems((prevItem) => res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const change = (e) => {
+    setValue(e.target.value);
+    console.log(e + ' ' + value);
+    //getItem();
+  };
+
+  const submitValue = (item) => {
+    setHero(item);
+    setItems([]);
+  };
+
   return (
-    <Autocomplete
-      title='Heroes'
-      onChange={(changedItem) => {
-        console.log(changedItem);
-      }}
-      items={['Apple', 'Apricot', 'Banana', 'Cherry', 'Cucumber']}
-    >
-      {(props) => {
-        const { getInputProps, getRef, inputValue } = props;
-
-        // await axios.get(`http://localhost:5000/api/autocomplete/${inputValue}`);
-        // let name = res.data.map((obj) => obj.name);
-        // console.log(name);
-
-        return (
-          <TextInput
-            placeholder='Search Heroes'
-            value={inputValue}
-            ref={getRef}
-            {...getInputProps()}
+    <>
+      <div className='wrapper'>
+        <div className='search-input'>
+          <a href='' target='_blank' hidden></a>
+          <input
+            type='text'
+            placeholder='Type to search..'
+            onKeyUp={(event) => {
+              if (event.code === 'Enter') getItem(event);
+              else change(event);
+            }}
           />
-        );
-      }}
-    </Autocomplete>
+          <div className='autocom-box'>
+            {items.map((item) => (
+              <li className='autoComplete' onClick={() => submitValue(item)}>
+                {item.name}
+              </li>
+            ))}
+          </div>
+          <div className='icon'>
+            <i className='fas fa-search' onClick={getItem}></i>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
