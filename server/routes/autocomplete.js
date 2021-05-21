bd1f86f4753319f1070710493ef2c90103353c3e
Abhinay Kumar <abhi01.amu@gmail.com>
2021-05-21 21:51:08 +0530

    try catch for autocomplete call

--- server/routes/autocomplete.js
+++ server/routes/autocomplete.js
@@ -7,32 +7,37 @@ const public_key = process.env.Public_Key;
 const private_key = process.env.Private_Key;
 
 const autocomplete = async (req, res, next) => {
-  const timestamp = new Date().getTime().toString();
-  const str = timestamp + private_key + public_key;
-  const hash = MD5(str).toString();
+  let timestamp = new Date().getTime().toString();
+  let str = timestamp + private_key + public_key;
+  let hash = MD5(str).toString();
   const { name } = req.params;
-  console.log(name);
+  console.log(name + ' ' + hash);
 
-  let response = await axios.get(
-    `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=75737056736c29d701a9ea7d7edeb778&hash=${hash}&nameStartsWith=${name}`
-  );
+  try {
+    let response = await axios.get(
+      `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=75737056736c29d701a9ea7d7edeb778&hash=${hash}&nameStartsWith=${name}`
+    );
 
-  if (response.data.code === 200) {
-    let obj = response.data.data.results.map((result) => {
-      const temp = {
-        id: result.id,
-        name: result.name,
-        path: result.thumbnail,
-      };
-      return temp;
-    });
+    if (response.data.code === 200) {
+      let obj = response.data.data.results.map((result) => {
+        const temp = {
+          id: result.id,
+          name: result.name,
+          path: result.thumbnail,
+        };
+        return temp;
+      });
 
-    console.log(obj);
-    res.json(obj);
-  } else {
-    res.status(400).json({
-      error: 'true',
-    });
+      console.log(obj);
+      res.json(obj);
+    } else {
+      res.status(400).json({
+        error: 'true',
+      });
+    }
+  } catch (err) {
+    console.log('abhinay');
+    console.log(err);
   }
 
   next();
